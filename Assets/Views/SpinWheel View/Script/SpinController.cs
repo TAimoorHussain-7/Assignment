535e115a8c5d0812cd56ed5ac5607d2be4a004dd
TAimoorHussain-7 <taimoor.hazelmobile@gmail.com>
2024-03-18 01:47:03 +0500

    Parsing Data From Json

--- Assets/Views/SpinWheel View/Script/SpinController.cs
+++ Assets/Views/SpinWheel View/Script/SpinController.cs
@@ -1,14 +1,22 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class SpinController : MonoBehaviour
 {
     [SerializeField] string PrefabName;
     [SerializeField] Transform RewardImageParent;
+    [SerializeField] int NumberOfSegments;
 
-    private void OnEnable()
+    [Button]
+    private void GenerateRewardImages()
     {
-        RewardImageMaker rImg = Instantiate(Resources.Load<RewardImageMaker>(PrefabName), RewardImageParent);
-        rImg.SetSegments(8);
+        float degreePerSegment = 360f / NumberOfSegments;
+        for (int i = 0; i < NumberOfSegments; i++)
+        {
+            RewardImageMaker rImg = Instantiate(Resources.Load<RewardImageMaker>(PrefabName), RewardImageParent);
+            rImg.transform.RotateAround(RewardImageParent.position, Vector3.forward, degreePerSegment * i);
+            rImg.SetSegments(NumberOfSegments);
+        }
     }
 
 }
